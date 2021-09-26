469348c972457674d4ab245680af5ff22c9cc7a8
doublegrey <sergejkropinov@gmail.com>
2021-09-26 20:29:25 +0300

    profile page

--- server.py
+++ server.py
@@ -1,6 +1,30 @@
 import os
-from http.server import HTTPServer, CGIHTTPRequestHandler
-os.chdir('./static')
-server_object = HTTPServer(server_address=(
-    '', 9000), RequestHandlerClass=CGIHTTPRequestHandler)
-server_object.serve_forever()
+from http.server import CGIHTTPRequestHandler, HTTPServer
+
+
+# server port (documentation: https://www.cloudflare.com/learning/network-layer/what-is-a-computer-port/)
+# port numbers 0 to 1023 are reserved for privileged services and designated as well-known ports
+# it's a good practice to use port numbers range from 1023 to 65535 for local servers
+PORT = 9000
+
+
+def start_server():
+    # change current working directory to './static'
+    os.chdir('./static')
+
+    server = HTTPServer(server_address=('', PORT),
+                        RequestHandlerClass=CGIHTTPRequestHandler)
+    # serve './static' folder
+    server.serve_forever()
+
+
+# Handle Ctrl+C event
+try:
+    print("Starting http server...")
+    start_server()
+
+# press Ctrl+C to stop server
+except KeyboardInterrupt:
+    print('\nShutting down http server...')
+    # exit code 0 means graceful shutdown
+    exit(0)
